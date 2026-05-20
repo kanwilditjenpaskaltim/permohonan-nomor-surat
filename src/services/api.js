dbc64e51ede11c30e129c0888067505a8989266d
Hendrizal <hendri.zzzzz@gmail.com>
2026-05-20 08:44:25 +0800

    update: api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
  * Konfigurasi API — Google Apps Script Web App
  */
 const GAS_BASE_URL =
-  "https://script.google.com/macros/s/AKfycbx3FLoxvCEwh_61cvVw4Y8NDnPQ4ahtyhyubk4gq-KROj52INidAIaouuCLu8W94zdX/exec";
+  "https://script.google.com/macros/s/AKfycbwB1L3TkiVOR6P_5hxcwtfoRimXTufU12tOjNlu68cOU_nfKcKcmc6hum4aTbjH_Nc/exec";
 
 /**
  * Cari nama pegawai berdasarkan NIP.
